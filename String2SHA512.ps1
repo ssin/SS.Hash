537b109b1ab001c0e0d0cd0e583c0d3b39052d30
SSin <ssinpublic-2012@yahoo.com.hk>
2013-02-19 05:42:28 +0800

    String to SHA512 powershell
    Added script for calculating SHA512

---
+++ String2SHA512.ps1
@@ -0,0 +1,5 @@
+﻿$string=""
+
+$crypto = [System.Security.Cryptography.SHA512]::Create()
+[BitConverter]::ToString($crypto.ComputeHash([system.Text.Encoding]::ASCII.GetBytes($string))).Replace("-","").ToLower()
+$crypto.Dispose()
